6858c26ca1261cca86382fa3795422a0254ac00e
AHorejsi <alex.horejsi59@gmail.com>
2021-03-13 10:09:20 -0800

    Figured out "Cannot resolve symbol R" error and adding some template code

--- LastMileProto/app/src/main/java/com/lastmile/lastmileproto/LoginActivity.java
+++ LastMileProto/app/src/main/java/com/lastmile/lastmileproto/LoginActivity.java
@@ -3,6 +3,7 @@ package com.lastmile.lastmileproto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView textView = findViewById(R.id.username);
+
+        textView.setOnClickListener(view -> {
+
+        });
     }
 }
