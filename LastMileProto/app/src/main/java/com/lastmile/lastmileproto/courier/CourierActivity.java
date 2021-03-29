2d0623bb8c454bc320ce4dfefac37b7dd684ef51
ca1ibear <mhorejsi3@gmail.com>
2021-03-29 01:24:12 -0700

    2/28/21 end of day

---
+++ LastMileProto/app/src/main/java/com/lastmile/lastmileproto/courier/CourierActivity.java
@@ -0,0 +1,16 @@
+package com.lastmile.lastmileproto.courier;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lastmile.lastmileproto.R;
+
+public class CourierActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_courier);
+    }
+}
